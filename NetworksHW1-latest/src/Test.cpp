4c4a0e453cecff90c1e31999f60463b37861f8af
notajourney <33099467+notajourney@users.noreply.github.com>
2019-11-28 13:24:37 +0200

    fourth  commit: Added 'Test' class under the correct hirarchy

---
+++ NetworksHW1-latest/src/Test.cpp
@@ -0,0 +1,9 @@
+/*
+ * Test.cpp
+ *
+ *  Created on: 28 бреб„ 2019
+ *      Author: Win10
+ */
+
+#include "Test.h"
+
---
+++ NetworksHW1-latest/src/Test.h
@@ -0,0 +1,14 @@
+/*
+ * Test.h
+ *
+ *  Created on: 28 бреб„ 2019
+ *      Author: Win10
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+class Test {//Added class inside source directory
+};
+
+#endif /* TEST_H_ */
